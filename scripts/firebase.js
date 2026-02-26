0932e82837e2dfd32f3ed65aa54b04b859828a78
uzairbinasif <shaikhuzairasif14@gmail.com>
2026-02-26 13:02:50 +0500

    first commit

---
+++ scripts/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYCVkHnmxj3hhV_sU4oiOVAc8Q7XJrqDQ",
+  authDomain: "mini-hackathon-smit-ed8b1.firebaseapp.com",
+  projectId: "mini-hackathon-smit-ed8b1",
+  storageBucket: "mini-hackathon-smit-ed8b1.firebasestorage.app",
+  messagingSenderId: "665917112342",
+  appId: "1:665917112342:web:7c29fe220499a3ddadf537"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
